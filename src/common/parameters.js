967d91f857f8ceccb66376b7ab84a306486516be
benco <benco1967@gmail.com>
2021-06-14 15:56:46 +0200

    change parameter after reinstall

--- src/common/parameters.js
+++ src/common/parameters.js
@@ -1,7 +1,7 @@
 export const URL_KIMAI = 'https://temps.lart-du-bois.com';
-export const ABSENCE_PROJECT_ID = 8;
+export const ABSENCE_PROJECT_ID = 1;
 export const isAbsenceProject = id => id === ABSENCE_PROJECT_ID;
 export const filterActivityByProject = id => activity =>
     (id === ABSENCE_PROJECT_ID && activity.id > 4) ||
     (id !== ABSENCE_PROJECT_ID && activity.id < 5);
-export const INSTALLATION_ACTIVITY_ID = 2;
+export const INSTALLATION_ACTIVITY_ID = 3;
